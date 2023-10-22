cfe11fce8e1a05236fac779dff710977cc29cbe4
Ale Susmelj <alesusmelj06@gmail.com>
2023-10-22 18:49:00 -0300

    Creacion de createClass controller

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -1,12 +1,36 @@
-var ClassService = require("../services/class.service");
+const ClassService = require("../services/class.service");
+const UserService = require("../services/user.service");
+const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
 
-// Esto va a crear una clase en la BD sin ningun usuario, ¿No tendriamos que matchear el usuario primero?
 exports.createClass = async function (req, res) {
+  const token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ status: 401, message: "Token no proporcionado" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    const userId = decoded.id;
+    const userBd = await User.findOne({ _id: userId });
+
+    if (!userBd) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "Usuario no encontrado" });
+    }
+  } catch (e) {
+    return res.json({ message: "Error al buscar usuario" });
+  }
+
   const {
     categoria,
     tipoClase,
     frecuencia,
-    //calificacion, No hay ninguna calificacion al crear el servicio?
     metodologia,
     costo,
     duracion,
@@ -17,24 +41,40 @@ exports.createClass = async function (req, res) {
     categoria,
     tipoClase,
     frecuencia,
+    calificacion: 0,
     metodologia,
     costo,
     duracion,
     descripcion,
-    activo: true, //Seteamos la clase como activa de entrada
-    comentarios: [], // Inicializamos la lista de comentarios vacia
+    activo: true,
+    comentarios: [],
   };
 
-  try {
-    const createdClase = await ClassService.createClase(user, clase);
-    return res
-      .status(201)
-      .json({ createdClase, message: "Succesfully created class" });
-  } catch (e) {
-    console.log(e);
-    return res
-      .status(400)
-      .json({ status: 400, message: "Class creation was unsuccesfull" });
+  if (
+    categoria &&
+    tipoClase &&
+    frecuencia &&
+    metodologia &&
+    costo &&
+    duracion &&
+    descripcion
+  ) {
+    try {
+      const createdClase = await ClassService.createClase(userBd, clase);
+      return res
+        .status(201)
+        .json({ createdClase, message: "Succesfully created class" });
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(400)
+        .json({ status: 400, message: "Class creation was unsuccesfull" });
+    }
+  } else {
+    return res.status(400).json({
+      status: 400,
+      message: "Se deben ingresar los campos obligatorios",
+    });
   }
 };
 
