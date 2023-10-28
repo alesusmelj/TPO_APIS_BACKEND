0a5bf595afefa4493c37852a38dc6ca85300c681
Ale Susmelj <alesusmelj06@gmail.com>
2023-10-28 20:38:55 -0300

    Endpoint createComment() finalizado

--- models/User.model.js
+++ models/User.model.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 // Schema para user
 const userSchema = new mongoose.Schema({
-  nombre: { type: String, required: true, min: 2, },
+  nombre: { type: String, required: true, min: 2 },
   apellido: { type: String, required: true, min: 2 },
   telefono: { type: String, required: true, min: 6 },
   mail: { type: String, required: true, unique: true, min: 6 },
@@ -20,15 +20,23 @@ const userSchema = new mongoose.Schema({
       duracion: Number,
       descripcion: String,
       activo: Boolean,
-      contrataciones: [{ mail: String, telefono: String, horario: String, motivo: String, estado: String, idNotificacion: String }],
+      contrataciones: [
+        {
+          mail: String,
+          telefono: String,
+          horario: String,
+          motivo: String,
+          estado: String,
+          idNotificacion: String,
+        },
+      ],
       comentarios: [
         {
           fecha: Date,
-          calificacion: Number,
+          estrellas: Number,
           mensaje: String,
           visto: Boolean,
           estado: String,
-          estrellas: Number
         },
       ],
     },
@@ -47,12 +55,12 @@ const userSchema = new mongoose.Schema({
       horario: String,
       idContacto: String,
       idComentario: String,
-      idServicio: String
-    }
-  ]
+      idServicio: String,
+    },
+  ],
 });
 
 // Crear modelos basados en los esquemas
 const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
--- routes/api/class.route.js
+++ routes/api/class.route.js
@@ -3,11 +3,12 @@ const ClassController = require("../../controllers/class.controller.js");
 const authorization = require("../../auth/auth.js");
 const router = express.Router();
 
-router.post("/create",/* authorization, */ ClassController.createClass);
+router.post("/create", /* authorization, */ ClassController.createClass);
 router.delete("/delete/:id", authorization, ClassController.deleteClass);
 router.put("/update/:id", authorization, ClassController.updateClass);
 router.put("/activate/:id", authorization, ClassController.activateClass);
 router.get("/:id", ClassController.getClass);
 router.post("/contact/:id", ClassController.contactUser);
+router.post("/comment/:id", ClassController.createComment);
 
 module.exports = router;
